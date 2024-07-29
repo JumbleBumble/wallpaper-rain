c083c5113947f402495e713a69a7288c0f9eab2c
Austin Sambrano <Austinsam210@gmail.com>
2024-07-29 16:45:33 -0400

    Remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import './App.css'
-import RainImages from './components/pages/RainWrapper'
+import RainImages from './RainWrapper'
 
 function App() {
 	return (
 		<div className="bg-black">
-			<RainImages
-				src="https://media-public.canva.com/56DI4/MAFIJu56DI4/1/tl.png"
-				gravity={20}
-				blackholeEffect={true}
-			>
+			<RainImages src="https://via.placeholder.com/50">
 				<div>Your content here</div>
 			</RainImages>
 		</div>
--- src/RainWrapper.tsx
+++ src/RainWrapper.tsx
@@ -182,6 +182,7 @@ const RainImages: React.FC<RainImagesProps> = ({
 		friction,
 		mass,
 		blackholeEffect,
+		maxVelocity,
 	])
 
 	return (
